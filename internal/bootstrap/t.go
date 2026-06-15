b79ba3b3fb3ba2ac1ec53f023e922cddd80ee71f
Arvind Sawant <arvind@ornaverse.com>
2026-06-15 19:49:17 +0530

    force English env locales for gotext to prevent Chinese fallback

--- internal/bootstrap/t.go
+++ internal/bootstrap/t.go
@@ -1,6 +1,8 @@
 package bootstrap
 
 import (
+	"os"
+
 	"github.com/leonelquinteros/gotext"
 
 	"github.com/acepanel/panel/v3/pkg/config"
@@ -8,6 +10,12 @@ import (
 )
 
 func NewT(conf *config.Config) (*gotext.Locale, error) {
+	if conf.App.Locale == "en" {
+		_ = os.Setenv("LANG", "en_US.UTF-8")
+		_ = os.Setenv("LC_ALL", "en_US.UTF-8")
+		_ = os.Setenv("LC_MESSAGES", "en_US.UTF-8")
+	}
+
 	l := gotext.NewLocaleFSWithPath(conf.App.Locale, embed.LocalesFS, "locales")
 	l.AddDomain("backend")
 
